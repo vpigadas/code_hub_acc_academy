2f60107e580adaf615a84cf84540455cedabbae1
vpigadas <vassilis.pigadas@gmail.com>
2021-02-11 09:01:30 +0200

    Course 2

--- app/src/main/java/com/codehub/academy/MainActivity.java
+++ app/src/main/java/com/codehub/academy/MainActivity.java
@@ -1,13 +1,49 @@
 package com.codehub.academy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        Button btn = findViewById(R.id.btn_login);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Button click");
+                Log.d("DemoApplication","Button click!!!");
+            }
+        });
+
+        TextView txn = findViewById(R.id.txt_login);
+
+        if(txn != null){
+            txn.setText("Vassilis");
+            txn.setText(getString(R.string.app_name));
+        }
+
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+
+        Button btn = findViewById(R.id.btn_login);
+
     }
 }
