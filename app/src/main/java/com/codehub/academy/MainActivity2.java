fd214515a67952d15e8e18b4cc4ffd51092dbde1
vpigadas <vassilis.pigadas@gmail.com>
2021-02-13 23:39:40 +0200

    Course 4

--- app/src/main/java/com/codehub/academy/MainActivity2.java
+++ app/src/main/java/com/codehub/academy/MainActivity2.java
@@ -21,7 +21,7 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
